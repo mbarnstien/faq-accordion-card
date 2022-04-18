edb87c90fb09783021cdb8bda0e8ecd3302dcb7b
Meg <92758663+mbarnstien@users.noreply.github.com>
2022-04-18 13:08:47 -0700

    Open transition working

--- js/script.js
+++ js/script.js
@@ -1,12 +1,37 @@
 document.querySelectorAll('.accordion-h').forEach((item) => {
     item.addEventListener('click', event => {
         console.log('click!');
+        let accCollapse = item.nextElementSibling;
+
+        if (!item.classList.contains('open')) {
+
+            accCollapse.style.display = 'block';
+            let accHeight = accCollapse.clientHeight;
+
+            setTimeout(() => {
+                accCollapse.style.height = accHeight + 'px';
+                accCollapse.style.display = '';
+            }, 1);
+
+            accCollapse.classList = 'accordion-collapse collapsing';
+
+            setTimeout(() => {
+                accCollapse.classList = 'accordion-collapse collapse open';
+                accCollapse.style.height = '';
+            }, 300);
+        } else {
+            accCollapse.classList = 'accordion-collapse collapse collapsing';
+
+            setTimeout(() => {
+                accCollapse.style.height = '0px';
+            }, 1);
+
+            setTimeout(() => {
+                accCollapse.classList = 'accordion-collapse collapse';
+                accCollapse.style.height = '';
+            }, 300);            
+        }
+
         item.classList.toggle('open');
-
-        //Add 'collapsing' class to .accordion-collapse (sibling)
-
-        //remove collapsing class and add 'open' class
-        
-        item.nextElementSibling.classList.toggle('open');
     });
 });
