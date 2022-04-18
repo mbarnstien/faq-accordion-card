661808b7b89ef5ce97fc9a494d79997e9c79a4e5
Meg <92758663+mbarnstien@users.noreply.github.com>
2022-04-18 11:46:38 -0700

    More CSS and basic Javascript functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('test');
+document.querySelectorAll('.accordion-h').forEach((item) => {
+    item.addEventListener('click', event => {
+        console.log('click!');
+        item.classList.toggle('open');
+
+        //Add 'collapsing' class to .accordion-collapse (sibling)
+
+        //remove collapsing class and add 'open' class
+        
+        item.nextElementSibling.classList.toggle('open');
+    });
+});
